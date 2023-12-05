55cac074cbee929fd01e58082983ee9c168f61ea
Acrylate1410 <quangnm1410@gmail.com>
2023-12-05 10:59:22 +0700

    sdf

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -17,7 +17,7 @@ export function Nav() {
           {text: "Liên hệ", link: "/lien-he"}].map(i => 
           <div key={i.text} className='relative text-center'  onMouseEnter={() => {i.text === "Sản phẩm" && setHover("")}} onMouseLeave={() => {i.text === "Sản phẩm" && setHover("hidden")}}>
             <Link reloadDocument to={i.link}><div className='cursor-pointer font-semibold hover:scale-105 transition mx-4 py-2'>{i.text}</div></Link>
-            {i.text === "Sản phẩm" && <div className={'z-[1000] absolute border border-gray-200 text-sm w-64 left-0 top-10 bg-white flex flex-col justify-center items-center pb-2 shadow-[0_30px_30px_-15px_rgba(0,0,0,0.3)] text-yellow-950 ' + isHover}>
+            {i.text === "Sản phẩm" && <div className={'z-[1000] absolute border border-gray-200 text-sm w-64 left-0 top-[41.5px] bg-white flex flex-col justify-center items-center pb-2 shadow-[0_30px_30px_-15px_rgba(0,0,0,0.3)] text-yellow-950 ' + isHover}>
                 <Link reloadDocument to="/danh-muc-san-pham?type=suc-khoe" className='py-2 cursor-pointer border-b border-black w-[90%] text-start hover:scale-105 hover:font-medium'>Sức khỏe</Link>
                 <Link reloadDocument to="/danh-muc-san-pham?type=lam-dep" className='py-2 cursor-pointer border-b border-black w-[90%] text-start hover:scale-105 hover:font-medium'>Làm đẹp</Link>
               </div>}

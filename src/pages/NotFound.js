90ebcb4f0a31c616e74ac2d2f31c48a99f76a474
Acrylate1410 <quangnm1410@gmail.com>
2023-12-06 14:14:27 +0700

    sdf

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 export function NotFound() {
   return (
     <div className="not-found">
-      <img src="404.png" alt="404" className="h-64 mx-auto mt-12"></img>
+      <img src="404.png" className="mx-auto mt-12"></img>
       <h1 className=" font-bold text-4xl text-center mx-2 mt-4 mb-8 text-yellow-950">Không tìm thấy trang bạn yêu cầu</h1>
       <Link to="/" className="link-home">
           <button className='mx-auto bg-yellow-950 text-white w-40 h-12 rounded-full flex items-center justify-center border border-yellow-950 hover:bg-white hover:text-[#093489] transition '>

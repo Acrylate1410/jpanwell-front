6e70f430b42a63a23e9753dd279ca6504105b748
Acrylate1410 <quangnm1410@gmail.com>
2023-12-14 10:36:24 +0700

    avoid layout shift

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -33,7 +33,7 @@ export function Home() {
             <img className='top-32' src="628.png"/>
           </div>
           <ImageEffect/>
-          <div className='mt-8 md:mt-12 overflow-y-hidden'>
+          <div className='mt-8 md:mt-12 h-[150px] md:h-[480px]'>
               <img className='w-full' src="banner2.png"></img>
           </div>
           <section className='w-full scroll-m-20  mt-6 '>

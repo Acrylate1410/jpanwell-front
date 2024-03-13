34c1d073bb2ceb631e7eb6be03a0d5a58f5da8b2
Acrylate1410 <quangnm1410@gmail.com>
2024-03-13 13:54:13 +0700

    sdf

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,14 +3,13 @@ import { Link, useSearchParams } from "react-router-dom";
 export default function Search() {
     const [results, setResults] = useState([])
     const [params] = useSearchParams()
-    let resultCount = 0
     useEffect(() => {
       fetch("https://jpanwell-api.onrender.com/products/get_products").then(res => res.json()).then(data => {
           setResults(data.filter(el => el.name.toLowerCase().includes(params.get("query").replaceAll("-", " "))) || [])
       }).catch(error => {})
     }, []);
     return (
-        <div className="w-[90%] md:w-1/2 mx-auto">
+        <div className="w-[90%] md:w-1/2 mx-auto mb-4">
             <h1 className="mt-4 text-lg font-semibold">{"Tìm thấy " + results.length + " kết quả"}</h1>
             {results.map(i =>
                 <Link reloadDocument className="flex items-center h-40 pl-2 py-1" to={"/san-pham?id=" + i.name.toLowerCase().replaceAll(" ", "-")}>

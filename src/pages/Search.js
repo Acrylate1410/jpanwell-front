1e59bc695ccc3e6c2f55d3177e9fd8c8057f0ca1
Acrylate1410 <quangnm1410@gmail.com>
2024-03-26 13:42:15 +0700

    dsf

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -12,12 +12,13 @@ export default function Search() {
     return (
         <div className="w-[90%] md:w-1/2 mx-auto mb-4">
             <h1 className="mt-4 text-lg font-semibold">{"Tìm thấy " + results.length + " kết quả"}</h1>
-            <div className="flex flex-wrap">
+
                 {!isFetch.current && <div className='mx-auto flex items-center justify-center'>
                 <div className='h-8 w-8 border border-y-black border-l-black rounded-full animate-spin'></div>
                 <div className='mx-2'></div>
                 <div>Đang tải sản phẩm</div>
               </div>}
+              <div>
                 {results.map(i =>
                     <Link reloadDocument className="flex items-center h-40 pl-2 py-1" to={"/san-pham?id=" + i.name.toLowerCase().replaceAll(" ", "-")}>
                         <div className="h-full aspect-square flex justify-center items-center"><img src={"/thumbnails/" + i.thumbnail} className="h-4/5 mx-auto"></img></div>
